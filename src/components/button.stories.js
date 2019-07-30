ce8a1e5f8905b32feb1a9372b27fb3ca46b80fd6
Tyler Hawkins <thawkins@instructure.com>
2019-07-30 14:37:13 -0600

    added Material UI Button examples

--- src/components/button.stories.js
+++ src/components/button.stories.js
@@ -1,10 +1,420 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import Button from '@material-ui/core/Button'
+import React from 'react';
+import clsx from 'clsx';
+import { storiesOf } from '@storybook/react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import SaveIcon from '@material-ui/icons/Save';
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import AlarmIcon from '@material-ui/icons/Alarm';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+  fab: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  leftIcon: {
+    marginRight: theme.spacing(1),
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+}));
+
+export function ContainedButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button variant="contained" className={classes.button}>
+        Default
+      </Button>
+      <Button variant="contained" color="primary" className={classes.button}>
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Secondary
+      </Button>
+      <Button variant="contained" color="secondary" disabled className={classes.button}>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons" className={classes.button}>
+        Link
+      </Button>
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" component="span" className={classes.button}>
+          Upload
+        </Button>
+      </label>
+    </div>
+  );
+}
 
 storiesOf('Button', module)
-    .add('Primary', () => (
-        <Button variant="contained" color="primary">
-            Hello World
+  .add('ContainedButtons', () => <ContainedButtons />);
+
+export function TextButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button className={classes.button}>Default</Button>
+      <Button color="primary" className={classes.button}>
+        Primary
         </Button>
-    ))
+      <Button color="secondary" className={classes.button}>
+        Secondary
+        </Button>
+      <Button disabled className={classes.button}>
+        Disabled
+        </Button>
+      <Button href="#text-buttons" className={classes.button}>
+        Link
+        </Button>
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="text-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="text-button-file">
+        <Button component="span" className={classes.button}>
+          Upload
+          </Button>
+      </label>
+    </div>
+  );
+}
+
+storiesOf('Button', module)
+  .add('TextButtons', () => <TextButtons />);
+
+export function OutlinedButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button variant="outlined" className={classes.button}>
+        Default
+        </Button>
+      <Button variant="outlined" color="primary" className={classes.button}>
+        Primary
+        </Button>
+      <Button variant="outlined" color="secondary" className={classes.button}>
+        Secondary
+        </Button>
+      <Button variant="outlined" disabled className={classes.button}>
+        Disabled
+        </Button>
+      <Button variant="outlined" href="#outlined-buttons" className={classes.button}>
+        Link
+        </Button>
+      <input
+        accept="image/*"
+        className={classes.input}
+        id="outlined-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="outlined-button-file">
+        <Button variant="outlined" component="span" className={classes.button}>
+          Upload
+          </Button>
+      </label>
+      <Button variant="outlined" color="inherit" className={classes.button}>
+        Inherit
+        </Button>
+    </div>
+  );
+}
+
+storiesOf('Button', module)
+  .add('OutlinedButtons', () => <OutlinedButtons />);
+
+export function GroupedButtons() {
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={6}>
+        <Grid container spacing={1} direction="column" alignItems="center">
+          <Grid item>
+            <ButtonGroup size="small" aria-label="small outlined button group">
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item>
+            <ButtonGroup color="primary" aria-label="outlined primary button group">
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item>
+            <ButtonGroup
+              color="secondary"
+              size="large"
+              aria-label="large outlined secondary button group"
+            >
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Grid container spacing={1} direction="column" alignItems="center">
+          <Grid item>
+            <ButtonGroup variant="contained" size="small" aria-label="small contained button group">
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item>
+            <ButtonGroup
+              variant="contained"
+              color="primary"
+              aria-label="full-width contained primary button group"
+            >
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item>
+            <ButtonGroup
+              variant="contained"
+              color="secondary"
+              size="large"
+              aria-label="large contained secondary button group"
+            >
+              <Button>One</Button>
+              <Button>Two</Button>
+              <Button>Three</Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <ButtonGroup fullWidth aria-label="full width outlined button group">
+          <Button>Full</Button>
+          <Button>width</Button>
+          <Button>ButtonGroup</Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
+  );
+}
+
+storiesOf('Button', module)
+  .add('GroupedButtons', () => <GroupedButtons />);
+
+
+export function SplitButton() {
+  const options = ['Create a merge commit', 'Squash and merge', 'Rebase and merge'];
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  function handleClick() {
+    alert(`You clicked ${options[selectedIndex]}`);
+  }
+
+  function handleMenuItemClick(event, index) {
+    setSelectedIndex(index);
+    setOpen(false);
+  }
+
+  function handleToggle() {
+    setOpen(prevOpen => !prevOpen);
+  }
+
+  function handleClose(event) {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  }
+
+  return (
+    <Grid container>
+      <Grid item xs={12} align="center">
+        <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+          <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+          <Button
+            color="primary"
+            variant="contained"
+            size="small"
+            aria-owns={open ? 'menu-list-grow' : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+          >
+            <ArrowDropDownIcon />
+          </Button>
+        </ButtonGroup>
+        <Popper open={open} anchorEl={anchorRef.current} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+              }}
+            >
+              <Paper id="menu-list-grow">
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList>
+                    {options.map((option, index) => (
+                      <MenuItem
+                        key={option}
+                        disabled={index === 2}
+                        selected={index === selectedIndex}
+                        onClick={event => handleMenuItemClick(event, index)}
+                      >
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </Grid>
+    </Grid>
+  );
+}
+
+storiesOf('Button', module)
+  .add('SplitButton', () => <SplitButton />);
+
+export function FloatingActionButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Fab color="primary" aria-label="add" className={classes.fab}>
+        <AddIcon />
+      </Fab>
+      <Fab color="secondary" aria-label="edit" className={classes.fab}>
+        <EditIcon />
+      </Fab>
+      <Fab variant="extended" aria-label="delete" className={classes.fab}>
+        <NavigationIcon className={classes.extendedIcon} />
+        Extended
+        </Fab>
+      <Fab disabled aria-label="delete" className={classes.fab}>
+        <DeleteIcon />
+      </Fab>
+    </div>
+  );
+}
+
+storiesOf('Button', module)
+  .add('FloatingActionButtons', () => <FloatingActionButtons />);
+
+export function IconLabelButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Delete
+          <DeleteIcon className={classes.rightIcon} />
+      </Button>
+      <Button variant="contained" color="primary" className={classes.button}>
+        Send
+          <SendIcon className={classes.rightIcon} />
+      </Button>
+      <Button variant="contained" color="default" className={classes.button}>
+        Upload
+          <CloudUploadIcon className={classes.rightIcon} />
+      </Button>
+      <Button variant="contained" disabled color="secondary" className={classes.button}>
+        <KeyboardVoiceIcon className={classes.leftIcon} />
+        Talk
+        </Button>
+      <Button variant="contained" size="small" className={classes.button}>
+        <SaveIcon className={clsx(classes.leftIcon, classes.iconSmall)} />
+        Save
+        </Button>
+    </div>
+  );
+}
+
+storiesOf('Button', module)
+  .add('IconLabelButtons', () => <IconLabelButtons />);
+
+export function IconButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <IconButton className={classes.button} aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
+      <IconButton className={classes.button} aria-label="delete" disabled color="primary">
+        <DeleteIcon />
+      </IconButton>
+      <IconButton color="secondary" className={classes.button} aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+      <IconButton color="primary" className={classes.button} aria-label="add to shopping cart">
+        <AddShoppingCartIcon />
+      </IconButton>
+      <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
+      <label htmlFor="icon-button-file">
+        <IconButton
+          color="primary"
+          className={classes.button}
+          aria-label="upload picture"
+          component="span"
+        >
+          <PhotoCameraIcon />
+        </IconButton>
+      </label>
+    </div>
+  );
+}
+
+storiesOf('Button', module)
+  .add('IconButtons', () => <IconButtons />);
